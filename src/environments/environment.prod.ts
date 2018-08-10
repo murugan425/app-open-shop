a77871188c4f9d1bfbeb671963e2a93c512cb5a9
Murugan Nagarajan <murugan425@gmail.com>
2018-08-10 20:07:31 +0800

    Trying to deploy in GAE - Google App Engine

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyCEEWoMznMPT9enoh3EPNXSWDKAnRTYrSI",
-    authDomain: "open-shop-murugan425.firebaseapp.com",
-    databaseURL: "https://open-shop-murugan425.firebaseio.com",
-    projectId: "open-shop-murugan425",
-    storageBucket: "open-shop-murugan425.appspot.com",
-    messagingSenderId: "348145461284"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,15 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyCEEWoMznMPT9enoh3EPNXSWDKAnRTYrSI",
-    authDomain: "open-shop-murugan425.firebaseapp.com",
-    databaseURL: "https://open-shop-murugan425.firebaseio.com",
-    projectId: "open-shop-murugan425",
-    storageBucket: "open-shop-murugan425.appspot.com",
-    messagingSenderId: "348145461284"
-  }
+  production: false
 };
 
 /*
